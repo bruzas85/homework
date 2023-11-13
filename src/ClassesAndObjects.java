950a865300bb7e8799d48afed408cabda8aaca08
Erik Bruzhas <bruzas.85@mail.ru>
2023-11-13 22:54:34 +0300

    Добавил класс person

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -2,6 +2,19 @@ public class ClassesAndObjects {
     public static void main(String[] args) {
         //Создание объекта класса car
     Car myCar = new Car();
+    //Создание объекта класса person
+    Person person1 = new Person();
+    Person person2 = new Person();
+
+    //установка значений класса person
+    person1.name = "Vasja";
+    person1.age = 45;
+    person2.name = "Andrey";
+    person2.age = 32;
+
+//Вывод значений класса person
+        System.out.println("Your neme is: "+person1.name+ " and your age: "+person1.age);
+
     //установка значений полей
         myCar.make = "Toyota";
         myCar.model = "Camry";
@@ -15,4 +28,9 @@ public class ClassesAndObjects {
         System.out.println("model: "+ myCar.model);
         System.out.println("year: "+ myCar.year);
     }
+}
+class Person{
+    String name;
+    int age;
+
 }
